19b7f8e288fe74782c53ec5c748c928e36cc0604
Happy-Birthday9 <websiterun904@gmail.com>
2026-08-09 19:53:41 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// =======================
+// Twelve Data Config
+// =======================
+
+const API_KEY = "f7569aaaff8c45b79dcc286bd1d5fec2";
+
+const API_URL = "https://api.twelvedata.com";
+
+// Default Symbol
+const DEFAULT_SYMBOL = "EUR/USD";
+
+// Default Interval
+const DEFAULT_INTERVAL = "1min";
